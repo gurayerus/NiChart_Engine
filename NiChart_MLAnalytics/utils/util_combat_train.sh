f5cd802d0f1651400266859e692de223a9f199bd
gurayerus <gurayerus@gmail.com>
2024-06-12 17:16:01 -0400

    Edit

---
+++ NiChart_MLAnalytics/utils/util_combat_train.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+## Read input
+in_csv=$1
+batch=$2
+vkey=$3
+vnum=$4
+vcat=$5
+vspline=$6
+out_mdl=$7
+out_csv=$8
+
+## Apply combat learn
+cmd="neuroharm -a learn -i $in_csv -k $vkey -b $batch -n $vnum -c $vcat -s $vspline -o $out_mdl -u $out_csv"
+echo "About to run: $cmd"
+$cmd
