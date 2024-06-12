21948ad487bc5a95ec0d930649abadb81c1f1ad4
gurayerus <gurayerus@gmail.com>
2024-06-12 15:16:15 -0400

    Edit

--- NiChart_MLAnalytics/workflows/w_SPARE/submit_slurm_snakemake.sh
+++ NiChart_MLAnalytics/workflows/w_SPARE/submit_slurm_snakemake.sh
@@ -4,10 +4,13 @@
 #SBATCH --output=slurm_logs/snakemake-%j.out
 
 source activate /cbica/home/erusg/.conda/envs/snakemake-tutorial
+# source activate /cbica/home/erusg/.conda/envs/spare
 
 cd /cbica/home/erusg/GitHub/NiChart/NiChart_Workflows/NiChart_Engine/NiChart_MLAnalytics/workflows/w_spare
 
 # snakemake --cores=1 --executor slurm --default-resources slurm_account=davatzic --jobs 1 
 snakemake --cores=1 --executor slurm --default-resources slurm_account=davatzic --jobs 10 
+
+# apptainer run ${SNAKEMAKE_DIR}/snakemake.sif --cores 1
 
 conda deactivate
