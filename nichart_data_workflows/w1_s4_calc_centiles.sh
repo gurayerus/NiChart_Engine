2827639e3e1d8c86d6dc3347b143b3bc10e328bc
gurayerus <gurayerus@example.com>
2024-10-16 22:56:56 -0400

    Edit centiles

---
+++ nichart_data_workflows/w1_s4_calc_centiles.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Workflow for preparing initial nichart data:
+
+flag=$1
+
+b_dir=$(dirname `pwd`)
+s_dir=${b_dir}/src/w_spare
+
+in_roi=${b_dir}/data/output/ISTAGING-v1.2/ISTAG_train_harmonized.csv
+in_demog=${b_dir}/data/output/ISTAGING-v1.2/ISTAG_DLMUSE_covars.csv
+out_dir=${b_dir}/data/output/ISTAGING-v1.2
+out_dset='ISTAG'
+res_dir=${b_dir}/resources
+
+cd ${s_dir}
+
+if [ "${flag}" == '--dry' ]; then
+    python calc_centiles.py --dry_run --in_roi $in_roi --in_demog $in_demog --out_dir $out_dir --out_dset $out_dset --res_dir $res_dir
+else
+    python calc_centiles.py --in_roi $in_roi --in_demog $in_demog --out_dir $out_dir --out_dset $out_dset --res_dir $res_dir
+fi
+
+
