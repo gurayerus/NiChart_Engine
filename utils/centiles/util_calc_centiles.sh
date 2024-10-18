f1ad5e24c9f4c3978b975737f114ab4d573b772d
gurayerus <gurayerus@gmail.com>
2024-10-18 13:58:17 -0400

    Change centile script to run for all vars in serial

--- utils/centiles/util_calc_centiles.sh
+++ utils/centiles/util_calc_centiles.sh
@@ -8,17 +8,10 @@ out_csv=$4
 # Load R module
 module load R/4.3
 
-## For each column after the first two (in csv format: MRID,Age,Var1,Var2,...)
-out_dir=`dirname $out_csv`
-out_tmp=${out_dir}/tmp_centiles_singlevar
-mkdir -pv ${out_tmp}
-
-for sel_var in `head -1 $in_csv | cut -d, -f3- | sed 's/,/ /g'`; do
-    echo "Calculating centiles for: $sel_var"
-    out_var=${out_tmp}/centiles_${sel_var}.csv
-    cmd="Rscript ../../utils/centiles/util_calc_centiles.r -i $in_csv -o $out_var -t $sel_var -c $cent_vals -b $bin_size -v -v -v"
-    echo "About to run: $cmd"
-    $cmd
+echo "Calculating centiles for: $in_csv"
+cmd="Rscript ../../utils/centiles/util_calc_centiles.r -i $in_csv -o $out_csv -c $cent_vals -b $bin_size -v"
+echo "About to run: $cmd"
+$cmd
 done
 
 
