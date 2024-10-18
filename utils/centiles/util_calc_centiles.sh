3dcb7c514e0f90ebbd8a73d3ac09c9de67e26e52
gurayerus <gurayerus@gmail.com>
2024-10-18 13:15:04 -0400

    Edit

--- utils/centiles/util_calc_centiles.sh
+++ utils/centiles/util_calc_centiles.sh
@@ -1,20 +1,23 @@
-#! /bin/bash
+#! /bin/bash -x
 
 in_csv=$1
-cent=$2
-bins=$3
-out_dir=$4
-out_csv=$5
+cent_vals=$2
+bin_size=$3
+out_csv=$4
 
 # Load R module
 module load R/4.3
 
 ## For each column after the first two (in csv format: MRID,Age,Var1,Var2,...)
+out_dir=`dirname $out_csv`
 out_tmp=${out_dir}/tmp_centiles_singlevar
 mkdir -pv ${out_tmp}
 
-for sel_var in `head -1 $in_csv | cut -d, -f3- | sed 's/,/ /g'; do
-    echo "Calculating centiles for: $sel_var
+for sel_var in `head -1 $in_csv | cut -d, -f3- | sed 's/,/ /g'`; do
+    echo "Calculating centiles for: $sel_var"
     cmd="Rscript ../../utils/centiles/util_calc_centiles.r -i $in -o $out -t $var -c $cent -b $bin -v"
     echo "About to run: $cmd"
     $cmd
+done
+
+
